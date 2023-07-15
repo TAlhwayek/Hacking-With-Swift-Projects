d86a0c4927c18983ce3105ad6fb43c5b4fdffe64
Tony Alhwayek <60246873+TAlhwayek@users.noreply.github.com>
2023-07-15 13:31:40 -0700

    Finished Project
    Includes completed challenges

--- Storm Viewer/Storm Viewer/ViewController.swift
+++ Storm Viewer/Storm Viewer/ViewController.swift
@@ -15,7 +15,10 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
         
+        // Use file manager to get the images
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
@@ -25,9 +28,33 @@ class ViewController: UITableViewController {
                 pictures.append(item)
             }
         }
-        print(pictures)
+        // Sort images in ascending order
+        pictures.sort()
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Generate needed rows
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        // Set row title as picture name
+        cell.textLabel?.text = pictures[indexPath.row]
+        // Return that cell
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            // Get index of selected image
+            vc.index = indexPath.row + 1
+            // Get total numbers of images
+            vc.imageCount = pictures.count
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 }
 
