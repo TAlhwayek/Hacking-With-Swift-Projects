29574ca3b46e98ade5f8679f255136b568f67d68
Tony Alhwayek <60246873+TAlhwayek@users.noreply.github.com>
2023-07-16 16:24:35 -0700

    Final commit - Project completed

--- Social Media/Storm Viewer/DetailViewController.swift
+++ Social Media/Storm Viewer/DetailViewController.swift
@@ -33,6 +33,9 @@ class DetailViewController: UIViewController {
         // Make image titles small
         navigationItem.largeTitleDisplayMode = .never
         
+        // Add navigation bar button
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
@@ -47,5 +50,16 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+
+        let vc = UIActivityViewController(activityItems: [image, selectedImage!], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
 }
