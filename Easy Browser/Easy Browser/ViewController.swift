5013737b0a80c8955cedcfeee465575008594fb9
Tony Alhwayek <60246873+TAlhwayek@users.noreply.github.com>
2023-07-16 16:44:23 -0700

    Added button that offers different websites
    Websites are apple, hackingwithswift, and my own github account.

--- Easy Browser/Easy Browser/ViewController.swift
+++ Easy Browser/Easy Browser/ViewController.swift
@@ -25,6 +25,9 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Add right-side navigation bar item
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
         // Assign value to URL
         let url = URL(string: "https://www.hackingwithswift.com")!
         
@@ -33,6 +36,26 @@ class ViewController: UIViewController, WKNavigationDelegate {
         
         // Enable gestures for easy-of-use
         webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    @objc func openTapped() {
+        // Add websites
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "github.com/TAlhwayek", style: .default, handler: openPage))
+            
+        // Dedicated cancel button
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        // Needed for iPads
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
     }
 
 
