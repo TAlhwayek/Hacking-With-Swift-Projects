956593440082994c02fad98038cdb80f0214a683
Tony Alhwayek <60246873+TAlhwayek@users.noreply.github.com>
2023-07-21 14:47:13 -0700

    First commit - Project created

---
+++ 7 Swifty Words/7 Swifty Words/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7 Swifty Words
+//
+//  Created by Tony Alhwayek on 7/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
