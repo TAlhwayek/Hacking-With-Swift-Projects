20262b42b82a8a6cdd4a511c8c1b2dcbe5348692
Tony Alhwayek <60246873+TAlhwayek@users.noreply.github.com>
2023-07-21 14:43:39 -0700

    Challenge #3 completed - Updated HTML
    Made a few changes to the present HTML section just to make everything look nicer. I believe the project is done at this point.

--- White House Petitions/White House Petitions/DetailViewController.swift
+++ White House Petitions/White House Petitions/DetailViewController.swift
@@ -27,10 +27,16 @@ class DetailViewController: UIViewController {
         <html>
         <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style> body { font-size: 150%; } </style>
+        <style> body {
+        font-size: 150%;
+        text-align: center;
+        } </style>
         <head>
         <body>
-        \(detailItem.body)
+        <font face = "Helvetica">
+        <h3>\(detailItem.title)</h3>
+        <p>\(detailItem.body)</p>
+        </font>
         </body>
         </html>
         """
