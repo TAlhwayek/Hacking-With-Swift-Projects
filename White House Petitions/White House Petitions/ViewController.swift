2c0ec9ad92093a5a0acbea39161d9c7e729c3e8d
Tony Alhwayek <60246873+TAlhwayek@users.noreply.github.com>
2023-07-20 22:22:28 -0700

    First commit - Project created

---
+++ White House Petitions/White House Petitions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  White House Petitions
+//
+//  Created by Tony Alhwayek on 7/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
