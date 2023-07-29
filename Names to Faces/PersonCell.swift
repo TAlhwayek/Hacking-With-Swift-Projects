e81742d5a415c8ab85475cf8f970a879ac73b144
Tony Alhwayek <60246873+TAlhwayek@users.noreply.github.com>
2023-07-28 20:45:42 -0700

    Setting up User Defaults project

---
+++ Names to Faces/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Names to Faces
+//
+//  Created by Tony Alhwayek on 7/24/23.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
