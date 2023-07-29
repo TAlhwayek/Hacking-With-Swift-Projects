7efbc94e94664ad375388350da9e98e0a104af01
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-07-28 23:54:01 -0700

    Completed challenge #1 from Project 12
    Added using user defaults to save how many times a picture was accessed. Also printing it console just for debugging purposes.

--- Storm Viewer/Storm Viewer/ViewController.swift
+++ Storm Viewer/Storm Viewer/ViewController.swift
@@ -11,7 +11,10 @@ class ViewController: UITableViewController {
     
     // Array to store pictures
     var pictures = [String]()
-
+    
+    // Array to count how many times each picture was accessed
+    var openCount = [Int](repeating: 0, count: 10)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,30 +34,60 @@ class ViewController: UITableViewController {
         }
         // Sort images in ascending order
         pictures.sort()
+        
+        // Save data using user defaults
+        let defaults = UserDefaults.standard
+        
+        if let savedCount = defaults.object(forKey: "count") as? Data {
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                openCount = try jsonDecoder.decode([Int].self, from: savedCount)
+            } catch {
+                print("Error decoding the array: \(error)")
+            }
+        }
+        
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // Generate needed rows
-        return pictures.count
+override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // Generate needed rows
+    return pictures.count
+}
+
+override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    // Create cell
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+    // Set row title as picture name
+    cell.textLabel?.text = pictures[indexPath.row]
+    // Return that cell
+    return cell
+}
+
+override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+        vc.selectedImage = pictures[indexPath.row]
+        // Get index of selected image
+        vc.index = indexPath.row + 1
+        // Increment the image's counter each time it's opened
+        openCount[indexPath.row] += 1
+        print("Image #\(indexPath.row + 1): \(openCount[indexPath.row])")
+        save()
+        // Get total numbers of images
+        vc.imageCount = pictures.count
+        navigationController?.pushViewController(vc, animated: true)
     }
+}
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // Create cell
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-        // Set row title as picture name
-        cell.textLabel?.text = pictures[indexPath.row]
-        // Return that cell
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
-            vc.selectedImage = pictures[indexPath.row]
-            // Get index of selected image
-            vc.index = indexPath.row + 1
-            // Get total numbers of images
-            vc.imageCount = pictures.count
-            navigationController?.pushViewController(vc, animated: true)
+    func save() {
+        let defaults = UserDefaults.standard
+        let jsonEncoder = JSONEncoder()
+        
+        do {
+            let savedCount = try jsonEncoder.encode(openCount)
+            defaults.set(savedCount, forKey: "count")
+        } catch {
+            print("Error enconding the array: \(error)")
         }
     }
 }
