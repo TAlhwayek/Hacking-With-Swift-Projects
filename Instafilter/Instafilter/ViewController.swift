8be5ca4c0b5f2bd44be4481b9f24ed7612a444e1
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-07-31 15:31:49 -0700

    First steps to adding a sepia filter

--- Instafilter/Instafilter/ViewController.swift
+++ Instafilter/Instafilter/ViewController.swift
@@ -5,17 +5,44 @@
 //  Created by Tony Alhwayek on 7/30/23.
 //
 
+import CoreImage
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet var imageView: UIImageView!
-    
     @IBOutlet var intensity: UISlider!
+    
+    // Display the main image
+    var currentImage: UIImage!
+    
+    // For filters
+    var context: CIContext!
+    var currentFilter: CIFilter!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Instafilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+        
+        // Apply a sepia filter
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
+    }
+    
+    // Allow getting user's photos
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        present(picker, animated: true)
+    }
+    
+    // When user chooses an image
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
     }
 
     @IBAction func changeFilter(_ sender: Any) {
@@ -24,6 +51,8 @@ class ViewController: UIViewController {
     @IBAction func save(_ sender: Any) {
     }
     
-    
+    // If the slider's value was changed
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
 }
 
