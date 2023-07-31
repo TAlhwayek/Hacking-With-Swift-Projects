db55d94cccb4fc74e1ce0e8928ad77f39f917cda
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-07-30 18:54:03 -0700

    First commit - Project created
    This course is very exciting and I love learning more about Swfit

---
+++ Instafilter/Instafilter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instafilter
+//
+//  Created by Tony Alhwayek on 7/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
