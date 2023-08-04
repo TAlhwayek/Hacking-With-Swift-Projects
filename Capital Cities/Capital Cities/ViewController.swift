97a2937d4a91b235aaa27d45bd303af812ff8ce9
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-08-03 20:20:38 -0700

    First commit - Project created

---
+++ Capital Cities/Capital Cities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Capital Cities
+//
+//  Created by Tony Alhwayek on 8/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
