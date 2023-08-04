65bff13c4dc63dbea69008aac5eb3e8bca31487a
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-08-03 20:34:56 -0700

    Added a few capital cities on the map

---
+++ Capital Cities/Capital Cities/Capital.swift
@@ -0,0 +1,21 @@
+//
+//  Capital.swift
+//  Capital Cities
+//
+//  Created by Tony Alhwayek on 8/3/23.
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String? = nil, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
