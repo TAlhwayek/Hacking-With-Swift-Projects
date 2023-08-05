3c6d76d5f8897ced7019617cbe815257096f9b69
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-08-05 14:37:25 -0700

    Added random objects and functional score

--- Space Race/Space Race/GameScene.swift
+++ Space Race/Space Race/GameScene.swift
@@ -13,6 +13,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
     var player: SKSpriteNode!
     var scoreLabel: SKLabelNode!
     
+    var possibleEnemies = ["wall", "hammer", "tv"]
+    var gameTimer: Timer?
+    var isGameOver = false
+    
     var score = 0 {
         didSet {
             scoreLabel.text = "Score: \(score)"
@@ -49,10 +53,42 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         physicsWorld.gravity = .zero
         // Get contact
         physicsWorld.contactDelegate = self
+        
+        // Configure timer between objects spawning
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.35, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
     }
     
+    @objc func createEnemy() {
+        // Get random enemy
+        guard let enemy = possibleEnemies.randomElement() else { return }
+        
+        let sprite = SKSpriteNode(imageNamed: enemy)
+        sprite.position = CGPoint(x: 1200, y: Int.random(in: 50...736))
+        addChild(sprite)
+        
+        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
+        // Allow collisions with player
+        sprite.physicsBody?.categoryBitMask = 1
+        // Make it move quickly on the x-axis (horizontally)
+        sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+        // Make it rotate
+        sprite.physicsBody?.angularVelocity = 5
+        // Does not slow down over time
+        sprite.physicsBody?.linearDamping = 0
+        // Never stop spinning
+        sprite.physicsBody?.angularDamping = 0
+    }
     
+    // Remove junk when it goes off screen
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        for node in children {
+            if node.position.x < -300 {
+                node.removeFromParent()
+            }
+        }
+        // When that happens, increment score
+        if !isGameOver {
+            score += 1
+        }
     }
 }
