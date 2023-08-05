c2498ad91c6f05233e4ccd1f328c714859640d57
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-08-05 15:15:27 -0700

    Challenge #3 done
    Objects no longer spawn after the game is over.

--- Space Race/Space Race/GameScene.swift
+++ Space Race/Space Race/GameScene.swift
@@ -65,40 +65,44 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         
     }
     
+    // Create random enemy
     @objc func createEnemy() {
-        // Get random enemy
-        guard let enemy = possibleEnemies.randomElement() else { return }
-        
-        let sprite = SKSpriteNode(imageNamed: enemy)
-        sprite.position = CGPoint(x: 1200, y: Int.random(in: 50...736))
-        addChild(sprite)
-        
-        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
-        // Allow collisions with player
-        sprite.physicsBody?.categoryBitMask = 1
-        // Make it move quickly on the x-axis (horizontally)
-        sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
-        // Make it rotate
-        sprite.physicsBody?.angularVelocity = 5
-        // Does not slow down over time
-        sprite.physicsBody?.linearDamping = 0
-        // Never stop spinning
-        sprite.physicsBody?.angularDamping = 0
-        
-        // For challenge #2
-        // Count created enemies
-        enemiesCreated += 1
-        print(enemiesCreated)
-        
-        // Reduce timer by 0.1 seconds after 20, 40, 60, etc. enemies are spawned
-        if enemiesCreated > 0 && enemiesCreated % 20 == 0 {
-            // Make sure we don't hit 0
-            if dynamicTimer > 0.1 {
-                // Decrease time between spawns
-                dynamicTimer -= 0.1
-                // Delete old timer and start new one with reduced interval
-                gameTimer?.invalidate()
-                gameTimer = Timer.scheduledTimer(timeInterval: dynamicTimer, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
+        // Challenge #3
+        // Stop creating space debris after game over
+        if !isGameOver {
+            guard let enemy = possibleEnemies.randomElement() else { return }
+            
+            let sprite = SKSpriteNode(imageNamed: enemy)
+            sprite.position = CGPoint(x: 1200, y: Int.random(in: 50...736))
+            addChild(sprite)
+            
+            sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
+            // Allow collisions with player
+            sprite.physicsBody?.categoryBitMask = 1
+            // Make it move quickly on the x-axis (horizontally)
+            sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+            // Make it rotate
+            sprite.physicsBody?.angularVelocity = 5
+            // Does not slow down over time
+            sprite.physicsBody?.linearDamping = 0
+            // Never stop spinning
+            sprite.physicsBody?.angularDamping = 0
+            
+            // For challenge #2
+            // Count created enemies
+            enemiesCreated += 1
+            print(enemiesCreated)
+            
+            // Reduce timer by 0.1 seconds after 20, 40, 60, etc. enemies are spawned
+            if enemiesCreated > 0 && enemiesCreated % 20 == 0 {
+                // Make sure we don't hit 0
+                if dynamicTimer > 0.1 {
+                    // Decrease time between spawns
+                    dynamicTimer -= 0.1
+                    // Delete old timer and start new one with reduced interval
+                    gameTimer?.invalidate()
+                    gameTimer = Timer.scheduledTimer(timeInterval: dynamicTimer, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
+                }
             }
         }
     }
