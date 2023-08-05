31a1d0dbffc94e18aa615e6de7d0aa7dd0d6d4e5
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-08-05 16:56:37 -0700

    Got country names to appear in list

--- Milestone #1 - Challenge/Milestone #1 - Challenge/ViewController.swift
+++ Milestone #1 - Challenge/Milestone #1 - Challenge/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    // Array to store flags
+    var flags = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Set title
+        title = "Flags"
+        
+        // Get all flags from assets
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        // Add all flags to array
+        for flag in items {
+            if flag.hasSuffix("@3x.png") {
+                flags.append(flag)
+                print("\(flag) appended")
+            }
+        }
+        
+        // Sort alphabetically
+        flags.sort()
     }
 
-
+    // Generate number of rows
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flags.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
+        cell.textLabel?.text = flags[indexPath.row]
+        return cell
+    }
+    
 }
 
