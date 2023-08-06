f5e78d43969880fbece0b197b878b494a9bd27b0
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-08-05 18:31:58 -0700

    Final commit - Projected completed
    Project completed as required, and also added swipe to delete gesture to make using the app more convenient

--- Milestone #2 - Challenge/Milestone #2 - Challenge/ViewController.swift
+++ Milestone #2 - Challenge/Milestone #2 - Challenge/ViewController.swift
@@ -7,13 +7,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    // Array to store added items
+    var shoppingList = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Shopping List"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(clearList))
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+    }
+    
+    // When add item button is pressed
+    @objc func addItem() {
+        let ac = UIAlertController(title: "Add item", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        // Capture text field's data
+        let addItem = UIAlertAction(title: "Add", style: .default) { [weak self, weak ac] _ in
+            guard let item = ac?.textFields?[0].text else { return }
+            self?.add(item)
+        }
+        ac.addAction(addItem)
+        present(ac, animated: true)
+    }
+    
+    // When clear button is pressed
+    @objc func clearList() {
+        shoppingList.removeAll()
+        tableView.reloadData()
+    }
+    
+    // Add item and refresh table
+    func add(_ item: String) {
+        shoppingList.insert(item, at: 0)
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+    }
+    
+    // Swipe to delete gesture
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            shoppingList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    
 }
 
