f778d557ab43129f976f5928ed2302498e185017
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-08-05 17:52:17 -0700

    First commit - Project created

---
+++ Milestone #2 - Challenge/Milestone #2 - Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone #2 - Challenge
+//
+//  Created by Tony Alhwayek on 8/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
