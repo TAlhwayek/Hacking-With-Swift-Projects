afd00ac3eed93bd1799b5a4ef57d4893e4a44dad
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-08-06 15:23:53 -0700

    Final commit - Project completed
    Not sure why I made a repo for this project but oh well.

--- Debugging/Debugging/ViewController.swift
+++ Debugging/Debugging/ViewController.swift
@@ -11,9 +11,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Prints
+        print("I'm inside the viewDidLoad() method")
+        print(1, 2, 3, 4, 5)
+        print(1, 2, 3, 4, 5, separator: "-")
+        print("Some message", terminator: "")
+        print("again")
+        print("Another message", terminator: "OOOOOOOOOOOOOO")
+        
+        // Assert
+        assert(1 == 1, "Math failure!")
+        // assert(1 == 2, "Math(2) failure!")
+        
+        // Breakpoint test
+        for i in 1...100 {
+            print("Got number \(i)")
+        }
     }
 
 
 }
 
+ 
