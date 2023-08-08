3093de3e0be416fb7bd369b47d4472559fba17a4
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-08-07 20:30:06 -0700

    Trigger notifications using a time interval now work

--- Local Notifications/Local Notifications/ViewController.swift
+++ Local Notifications/Local Notifications/ViewController.swift
@@ -5,15 +5,59 @@
 //  Created by Tony Alhwayek on 8/7/23.
 //
 
+import UserNotifications
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
     }
-
-
+    
+    @objc func registerLocal() {
+        // Get permission for notifications
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if granted {
+                print("Yay")
+            } else {
+                print("D'oh!")
+            }
+            
+        }
+    }
+    
+    @objc func scheduleLocal() {
+        let center = UNUserNotificationCenter.current()
+        // Remove all pending notifications (haven't been sent yet)
+        center.removeAllPendingNotificationRequests()
+        
+        let content = UNMutableNotificationContent()
+        // Set what the notification will show
+        content.title = "Late wake up call"
+        content.body = "The early bird catches the worm, but the second mouse gets the cheese."
+        // Set a category
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["customData": "fizzBuzz"]
+        // Set default notification sound
+        content.sound = .default
+        
+        // Specify time to schedule alarm
+        var dateComponents = DateComponents()
+        dateComponents.hour = 20
+        dateComponents.minute = 25
+        // let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        // Show alarm after 5 seconds
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
+    }
+    
 }
 
