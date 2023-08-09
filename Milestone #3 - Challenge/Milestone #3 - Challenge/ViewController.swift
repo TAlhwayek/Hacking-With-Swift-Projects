fe67983fad45ca900d41d3552751c792954b19cb
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-08-08 19:30:01 -0700

    Lots of features added

--- Milestone #3 - Challenge/Milestone #3 - Challenge/ViewController.swift
+++ Milestone #3 - Challenge/Milestone #3 - Challenge/ViewController.swift
@@ -11,13 +11,43 @@ class ViewController: UIViewController {
     
     // Label that displays current word and progress
     @IBOutlet var wordTitle: UILabel!
-    
+    // Display how many lives the user still has
+    @IBOutlet var livesRemainingLabel: UILabel!
     // Store words
     var words = [String]()
     // Array to store used letters
     var usedLetters = [String]()
     // Count wrong answers
     var wrongAnswerCount = 0
+    
+    // Change color of livesRemainingLabel based on lives left
+    var colors = [".red", ".red", ".orange", ".yellow", ".yellow", ".green", ".green"]
+    // Define a mapping between color strings and UIColors
+    let colorMapping: [String: UIColor] = [
+        ".red": UIColor.red,
+        ".orange": UIColor.orange,
+        ".yellow": UIColor.yellow,
+        ".green": UIColor.green
+    ]
+    // How many lives the user still has
+    var livesRemaining = 2 {
+        didSet {
+            livesRemainingLabel.text = "Lives remaining: \(livesRemaining)"
+            let colorString = colors[livesRemaining - 1]
+            if let color = colorMapping[colorString] {
+                livesRemainingLabel.textColor = color
+            }
+        }
+    }
+    
+    // Current word to guess
+    var currentWord: String = ""
+    // The word, but hidden
+    var hiddenWord: String = "" {
+        didSet {
+            wordTitle.text = "\(hiddenWord)"
+        }
+    }
     
 
     override func viewDidLoad() {
@@ -38,16 +68,22 @@ class ViewController: UIViewController {
         
         title = "Hang the Dead Man"
         
-        print("PRINTING WORDS")
-  //      for word in words {
-            print(words)
-    //    }
+        newGame()
     }
 
     // Function that gets a new word from the list and starts a new game
     @objc func newGame() {
         // Clear used letters
         usedLetters.removeAll()
+        livesRemaining = 7
+        // Get new word
+        currentWord = words.randomElement()!
+        // Initialize hidden word and use that in the game
+        hiddenWord = String(repeating: "?", count: currentWord.count)
+        
+        // DEBUGGING REMEMBER TO REMOVE
+        print(currentWord)
+        print(hiddenWord)
     }
     
     // Let user add letter
@@ -57,20 +93,84 @@ class ViewController: UIViewController {
         // Add a text field
         wordAC.addTextField()
         
+        // When user submits an answer
         let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak wordAC] _ in
             // Get letter
             guard let submittedLetter = wordAC?.textFields?[0].text else { return }
-            print(submittedLetter)
             // Submit letter
-            // self?.submit(submittedLetter)
+            self?.submit(submittedLetter)
         }
         
         wordAC.addAction(submitAction)
         present(wordAC, animated: true)
     }
     
-    func checkCharacterCount() {
+    // Make sure only one letter was submitted
+    func checkCharacterCount(letter: String) -> Bool {
+        return letter.count == 1
+    }
+    
+    // Make sure user only submits letters
+    func containsOnlyLetters(letter: String) -> Bool {
+        let onlyAllowLetters = CharacterSet.letters
+        return letter.unicodeScalars.allSatisfy { onlyAllowLetters.contains($0)}
+    }
+    
+    func isUsedLetter(letter: String) -> Bool {
+        return usedLetters.contains(letter)
+    }
+    
+    // Check if the letter is part of the word
+    func isACorrectLetter(letter: String) -> Bool {
+        // Check if original word contains
+        // Then replace position in hiddenWord
         
+        if let index = currentWord.firstIndex(of: Character(letter)) {
+            
+        }
+    }
+    
+    // Main submit functionality
+    // Makes sure letter passes all checks
+    func submit(_ letter: String) {
+        // Check if only ONE character was submitted
+        if checkCharacterCount(letter: letter) {
+            // Check if input contains a letter and not a number, punctuation, etc.
+            if containsOnlyLetters(letter: letter) {
+                // Check if letter was not already used
+                if !isUsedLetter(letter: letter) {
+                    // Check if letter is part of the solution
+                    if isACorrectLetter(letter: letter) {
+                        
+                    } else {
+                        // Deduct a life for an incorrect answer
+                        livesRemaining -= 1
+                        
+                        // Check if game over
+                        if livesRemaining == 0 {
+                            let gameOverAC = UIAlertController(title: "Game Over", message: "You ran out of lives", preferredStyle: .alert)
+                            let okAction = UIAlertAction(title: "Try again?", style: .default) { _ in
+                                // Start new game
+                                self.newGame()
+                            }
+                            gameOverAC.addAction(okAction)
+                            present(gameOverAC, animated: true)
+                        } else {
+                            showErrorAC(title: "Wrong Answer", message: "")
+                        }
+                    }
+                } else {
+                    showErrorAC(title: "Letter already used", message: "Please use a different letter")
+                }
+            } else {
+                showErrorAC(title: "Invalid input", message: "Please input a LETTER")
+            }
+        } else {
+            showErrorAC(title: "Invalid input", message: "Please input only ONE letter")
+        }
+        
+        
+       
     }
     
     // Show an alert controller when needed
