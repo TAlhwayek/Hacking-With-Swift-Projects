68cd00b4f303dc3d688d11954bc7506322a3d2d5
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-08-09 22:18:15 -0700

    Project supposedly done. Can't test it.
    Maybe because I'm on the beta version of iOS, but the app is not working and it won't load on my iPad. I will come back to it later if i remember.

--- Detect-a-Beacon/Detect-a-Beacon/ViewController.swift
+++ Detect-a-Beacon/Detect-a-Beacon/ViewController.swift
@@ -47,5 +47,38 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
         locationManager?.startMonitoring(for: beaconRegion)
         locationManager?.startRangingBeacons(in: beaconRegion)
     }
+    
+    // Update background and text based on distance of beacon
+    func update(distance: CLProximity) {
+        UIView.animate(withDuration: 1) {
+            switch distance {
+            
+            case .far:
+                self.view.backgroundColor = .blue
+                self.distanceReading.text = "FAR"
+                
+            case .near:
+                self.view.backgroundColor = .orange
+                self.distanceReading.text = "NEAR"
+                
+            case .immediate:
+                self.view.backgroundColor = .red
+                self.distanceReading.text = "IMMEDIATE"
+            
+            default:
+                self.view.backgroundColor = .gray
+                self.distanceReading.text = "UNKNOWN"
+            }
+        }
+    }
+    
+    // Get location of beacon
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        if let beacon = beacons.first {
+            update(distance: beacon.proximity)
+        } else {
+            update(distance: .unknown)
+        }
+    }
 }
 
