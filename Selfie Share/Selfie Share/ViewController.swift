2d5f6e0c71fc71512f66a49b474f8b2625d3d4ad
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-08-12 17:53:46 -0700

    First commit - Project created

---
+++ Selfie Share/Selfie Share/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Selfie Share
+//
+//  Created by Tony Alhwayek on 8/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
