2cccb3decd1c004a5e01b1cfddc7963e5fba3f10
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-08-12 18:46:24 -0700

    Project completed. Now for the challanges.
    🫡

--- Selfie Share/Selfie Share/ViewController.swift
+++ Selfie Share/Selfie Share/ViewController.swift
@@ -5,11 +5,66 @@
 //  Created by Tony Alhwayek on 8/12/23.
 //
 
+import MultipeerConnectivity
 import UIKit
 
-class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, MCSessionDelegate, MCBrowserViewControllerDelegate {
+    // Functions needed for last 2 delegates
+    // When it successfully finishes
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+    
+    // When it is cancelled
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+    
+    // When session state changes
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        switch state {
+        case .connected:
+            print("Connected: \(peerID.displayName)")
+        case .connecting:
+            print("Connecting: \(peerID.displayName)")
+        case .notConnected:
+            print("Not Connected: \(peerID.displayName)")
+        @unknown default:
+            print("Unknown state received: \(peerID.displayName)")
+        }
+    }
+    
+    // When data is received
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        DispatchQueue.main.async { [weak self ] in
+            if let image = UIImage(data: data) {
+                // Add image and reload collection view
+                self?.images.insert(image, at: 0)
+                self?.collectionView.reloadData()
+            }
+        }
+    }
+    
+    // Intentionally kept empty
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+    }
+    
+    // Intentionally kept empty
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+    }
+    
+    // Intentionally kept empty
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+    }
+    
     // Imported images
     var images = [UIImage]()
+    
+    // Needed for peer-to-peer connections
+    // Get the name of the current device
+    var  peerID = MCPeerID(displayName: UIDevice.current.name)
+    var mcSession: MCSession?
+    var mcAdvertiserAssitant: MCAdvertiserAssistant?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +73,12 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         title = "Selfie Share"
         // Import picture button
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+        // Show connection button
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showConnectionPrompt))
+        
+        // Create new session using peerID, and encrypt it
+        mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
+        mcSession?.delegate = self
     }
     
     // Set number of collection views
@@ -36,7 +97,33 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         return cell
     }
 
+    // When an image is selected in the image picker
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        // Add image to collection view
+        images.insert(image, at: 0)
+        collectionView.reloadData()
+        
+        // Multipeer code
+        guard let mcSession = mcSession else { return }
+        // If at least one peer is connected
+        if mcSession.connectedPeers.count > 0 {
+            if let imageData = image.pngData() {
+                // Try sending image
+                do {
+                    try mcSession.send(imageData, toPeers: mcSession.connectedPeers, with: .reliable)
+                } catch {
+                    let ac = UIAlertController(title: "Error sending image", message: error.localizedDescription, preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "OK", style: .default))
+                    present(ac, animated: true)
+                }
+            }
+        }
+    }
     
+    // Bar button item functions
+    // Import picture on button press
     @objc func importPicture() {
         let picker = UIImagePickerController()
         picker.allowsEditing = true
@@ -44,13 +131,30 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         present(picker, animated: true)
     }
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        guard let image = info[.editedImage] as? UIImage else { return }
-        dismiss(animated: true)
-        
-        images.insert(image, at: 0)
-        collectionView.reloadData()
+    // Allow users to connect to each other
+    @objc func showConnectionPrompt() {
+        let ac = UIAlertController(title: "Connect to others", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Host a session", style: .default, handler: startHosting))
+        ac.addAction(UIAlertAction(title: "Join a session", style: .default, handler: joinSession))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
     }
-
+    
+    // Functions for peer-to-peer
+    // Start hosting a session
+    func startHosting(action: UIAlertAction) {
+        guard let mcSession = mcSession else { return }
+        mcAdvertiserAssitant = MCAdvertiserAssistant(serviceType: "hws-project-25", discoveryInfo: nil, session: mcSession)
+        // Start looking for connections
+        mcAdvertiserAssitant?.start()
+    }
+    
+    // Join an existing session
+    func joinSession(action: UIAlertAction) {
+        guard let mcSession = mcSession else { return }
+        let mcBrowser = MCBrowserViewController(serviceType: "hws-project-25", session: mcSession)
+        mcBrowser.delegate = self
+        present(mcBrowser, animated: true)
+    }
 }
 
