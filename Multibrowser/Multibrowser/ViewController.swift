eb385369a4749e9caccd926410d4ee7ee00b9a65
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-08-15 21:46:50 -0700

    Added interface builder elements
    Added a URL bar (text field) along with a stack view.

--- Multibrowser/Multibrowser/ViewController.swift
+++ Multibrowser/Multibrowser/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var addressBar: UITextField!
+    @IBOutlet var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
 
