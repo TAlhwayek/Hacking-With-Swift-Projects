dfffdf1f9aba13fde8be67092a1124c9a8b025ac
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-08-17 02:25:46 -0700

    Each row now displays info when pressed

--- Milestone #5 - Challenge/Milestone #5 - Challenge/DetailViewController.swift
+++ Milestone #5 - Challenge/Milestone #5 - Challenge/DetailViewController.swift
@@ -8,19 +8,49 @@
 import UIKit
 
 class DetailViewController: UITableViewController {
-
+    
     //var flag: UIImage!
-    var name: String!
-    var capital: String!
-    var yearEstablished: Int!
-    var population: String!
-    var coordinates: Coordinates!
+    var name: String?
+    var capital: String?
+    var yearEstablished: Int?
+    var population: String?
+    var coordinates: Coordinates?
+    
+    var countryInfo: Country?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        countryInfo = Country(name: name!, capital: capital!, yearEstablished: yearEstablished!, population: population!, coordinates: coordinates!)
+        
         title = name
-        // Do any additional setup after loading the view.
     }
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
+        switch indexPath.row {
+              case 0:
+                  cell.textLabel?.text = "Name: \(countryInfo!.name)"
+                  
+              case 1:
+                  cell.textLabel?.text = "Capital: \(countryInfo!.capital)"
+                  
+              case 2:
+                  cell.textLabel?.text = "Year Established: \(countryInfo!.yearEstablished)"
+                  
+              case 3:
+                  cell.textLabel?.text = "Population: \(countryInfo!.population)"
+                  
+              case 4:
+                  cell.textLabel?.text = "Coordinates: [\(countryInfo!.coordinates.longitude), \(countryInfo!.coordinates.latitude)]"
+                  
+              default:
+                  break
+              }
+            
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
 }
