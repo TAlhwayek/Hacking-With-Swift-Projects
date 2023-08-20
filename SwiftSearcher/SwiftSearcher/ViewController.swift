682d8a7aac7dd805c51cfd3da194198764015f81
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-08-20 03:15:42 -0700

    First commit - Project created

---
+++ SwiftSearcher/SwiftSearcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftSearcher
+//
+//  Created by Tony Alhwayek on 8/20/23.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
