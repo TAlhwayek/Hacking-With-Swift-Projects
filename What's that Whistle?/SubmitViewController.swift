016ff5979dec09e16c36771bd7feb544f5ebdc32
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-08-23 22:03:04 -0700

    Cannot proceed without iCloud, which requires paid dev account
    Sad sad day for me. Was very excited about this project.

---
+++ What's that Whistle?/SubmitViewController.swift
@@ -0,0 +1,76 @@
+//
+//  SubmitViewController.swift
+//  What's that Whistle?
+//
+//  Created by Tony Alhwayek on 8/23/23.
+//
+
+import CloudKit
+import UIKit
+
+class SubmitViewController: UIViewController {
+    
+    var genre: String!
+    var comments: String!
+    
+    var stackView: UIStackView!
+    var status: UILabel!
+    var spinner: UIActivityIndicatorView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "You're all set!"
+        // Hide back button once submission is finished
+        navigationItem.hidesBackButton = true
+    }
+    
+    // Set up the UI
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .gray
+        
+        stackView = UIStackView()
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = UIStackView.Distribution.fillEqually
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+        
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        status = UILabel()
+        status.translatesAutoresizingMaskIntoConstraints = false
+        status.text = "Submitting..."
+        status.textColor = .white
+        status.font = UIFont.preferredFont(forTextStyle: .title1)
+        status.numberOfLines = 0
+        status.textAlignment = .center
+        
+        spinner = UIActivityIndicatorView(style: .large)
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.hidesWhenStopped = true
+        spinner.startAnimating()
+        
+        stackView.addArrangedSubview(status)
+        stackView.addArrangedSubview(spinner)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        doSubmission()
+    }
+    
+    func doSubmission() {
+        
+    }
+    
+    // Go back to root view controller
+    @objc func doneTapped() {
+        _ = navigationController?.popToRootViewController(animated: true)
+    }
+}
