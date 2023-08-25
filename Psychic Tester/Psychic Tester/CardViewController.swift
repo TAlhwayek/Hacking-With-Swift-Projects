ceeec3189fa3a0a043694de90c6657eb753b78fe
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-08-24 22:35:03 -0700

    First commit - Project created

---
+++ Psychic Tester/Psychic Tester/CardViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CardViewController.swift
+//  Psychic Tester
+//
+//  Created by Tony Alhwayek on 8/24/23.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
