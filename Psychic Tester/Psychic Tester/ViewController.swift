57b24a74b97d8b9ed84e9b56b25133f870e25c4e
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-08-25 16:14:09 -0700

    Fixed background and added card wiggle

--- Psychic Tester/Psychic Tester/ViewController.swift
+++ Psychic Tester/Psychic Tester/ViewController.swift
@@ -5,12 +5,13 @@
 //  Created by Tony Alhwayek on 8/24/23.
 //
 
+import AVFoundation
 import UIKit
 
 var allCards = [CardViewController]()
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var cardContainer: UIView!
     
     @IBOutlet var gradientView: GradientView!
@@ -19,6 +20,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        createParticles()
         loadCards()
         
         view.backgroundColor = .red
@@ -27,7 +29,7 @@ class ViewController: UIViewController {
             self.view.backgroundColor = .blue
         })
     }
-
+    
     @objc func loadCards() {
         // Remove previous cards
         for card in allCards {
@@ -41,14 +43,14 @@ class ViewController: UIViewController {
         
         // Create an array of card positions
         let positions = [
-        CGPoint(x: 75, y: 85),
-        CGPoint(x: 185, y: 85),
-        CGPoint(x: 295, y: 85),
-        CGPoint(x: 405, y: 85),
-        CGPoint(x: 75, y: 235),
-        CGPoint(x: 185, y: 235),
-        CGPoint(x: 295, y: 235),
-        CGPoint(x: 405, y: 235)
+            CGPoint(x: 75, y: 85),
+            CGPoint(x: 185, y: 85),
+            CGPoint(x: 295, y: 85),
+            CGPoint(x: 405, y: 85),
+            CGPoint(x: 75, y: 235),
+            CGPoint(x: 185, y: 235),
+            CGPoint(x: 295, y: 235),
+            CGPoint(x: 405, y: 235)
         ]
         
         // Load and unwrap card images
@@ -103,9 +105,45 @@ class ViewController: UIViewController {
         perform(#selector(loadCards), with: nil, afterDelay: 2)
     }
     
-  
+    /*
+     The birthRate property sets how many particles to create every second.
+     The lifetime property sets how long each particle should live, in seconds.
+     The velocity property sets the base movement speed for each particle.
+     The velocityRange property sets how much velocity variation there can be.
+     The emissionLongitude property sets the direction particles are fired.
+     The spinRange property sets how much spin variation there can be between particles.
+     The scale property sets how large particles should be, where 1.0 is full size.
+     The scaleRange property sets how much size variation there can be between particles.
+     The color property sets the color to be applied to each particle.
+     The alphaSpeed property sets how fast particles should be faded out (or in) over their lifetime.
+     The contents property assigns a CGImage to be used as the image.
+     */
+    func createParticles() {
+        let particleEmitter = CAEmitterLayer()
+        
+        particleEmitter.emitterPosition = CGPoint(x: view.frame.width / 2.0, y: -50)
+        particleEmitter.emitterShape = .line
+        particleEmitter.emitterSize = CGSize(width: view.frame.width, height: 1)
+        particleEmitter.renderMode = .additive
+        
+        let cell = CAEmitterCell()
+        cell.birthRate = 2
+        cell.lifetime = 5
+        cell.velocity = 100
+        cell.velocityRange = 50
+        cell.emissionLongitude = .pi
+        cell.spinRange = 5
+        cell.scale = 0.5
+        cell.scale = 0.25
+        cell.color = UIColor(white: 1, alpha: 0.1).cgColor
+        cell.alphaSpeed = -0.025
+        cell.contents = UIImage(named: "particle")?.cgImage
+        particleEmitter.emitterCells = [cell]
+        gradientView.layer.addSublayer(particleEmitter)
+    }
     
     
-
+    
+    
 }
 
